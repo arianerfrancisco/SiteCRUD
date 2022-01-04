4e2669784fae91e64e757b0db2b7321a2f79d834
Ariane Rocha <72419533+arianerfrancisco@users.noreply.github.com>
2022-01-04 18:10:40 -0300

    Crud

--- main.js
+++ main.js
@@ -6,6 +6,34 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempCliente = {
+    nome:"Mel",
+    email: "mel@email.com",
+    celular: "1122333455",
+    cidade: "Londrina"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(db_client))
+
+// CRUD - UPDATE
+const updateClient = (index, client) => {
+    const dbClient = readClient()
+    dbClient[index] = client
+    setLocalStorage(dbClient)
+}
+
+// CRUD - READ
+const readClient = () => getLocalStorage()
+
+// CRUD- CREAT
+const CreateClient = (client) => {
+    const dbClient  = getLocalStorage(client)
+    dbClient.push(client)
+    setLocalStorage(dbClient)
+}
+
+
 // eventos
 
 document.getElementById('cadastrarCliente')
@@ -14,7 +42,3 @@ document.getElementById('cadastrarCliente')
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
 
-
-// CRUD
-
-const CreateClient = (cliente)
